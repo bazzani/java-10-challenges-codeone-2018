8c0fa375559f462c9e8792e29f21fbda3582aeb2
Barry Evans <barryevans80@gmail.com>
2018-09-23 11:44:42 +0100

    Add var challenge draft - add newInstance call

--- src/main/java/the/java/challenges/VarChallenge.java
+++ src/main/java/the/java/challenges/VarChallenge.java
@@ -1,16 +1,16 @@
 package the.java.challenges;
 
 public class VarChallenge {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
         var myVar = new Object();
         var yourVar = new Object(){public Integer doit(){return 1;}};
         var theirVar = new Object(){public Integer doit(){return 1;}};
-        var otherVar = yourVar;
+        var otherVar = yourVar.getClass().newInstance();
 
         boolean c1 = myVar.getClass().equals(yourVar.getClass());
         boolean c2 = yourVar.getClass().equals(theirVar.getClass());
-        boolean c3 = otherVar.getClass().equals(myVar.getClass());
-        boolean c4 = yourVar.doit().equals(otherVar.doit());
+        boolean c3 = otherVar.getClass().equals(yourVar.getClass());
+        boolean c4 = otherVar.getClass().equals(Object.class);
 
         System.out.println(c1 + ":" + c2 + ":" + c3 + ":" + c4);
     }
