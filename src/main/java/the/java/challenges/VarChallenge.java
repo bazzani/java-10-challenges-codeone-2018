c3ab298dd947370a0f101ff3188ed22cac3131d4
Barry Evans <bazzani@users.noreply.github.com>
2018-10-01 19:25:44 +0100

    Add blog updates about null and var keyword
    Todo compare with null
    
    Non-Denotable types (like Anonymous Classes) can be inferred by var.
    
    var is not a keyword boolean var = false;

--- src/main/java/the/java/challenges/VarChallenge.java
+++ src/main/java/the/java/challenges/VarChallenge.java
@@ -5,21 +5,23 @@ import java.lang.reflect.InvocationTargetException;
 import static java.lang.String.join;
 
 public class VarChallenge {
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        var myVar = new Object();
-        var yourVar = new Object(){public Integer doit(){return 1;}};
-        var theirVar = new Object(){public Integer doit(){return 1;}};
-        var otherVar = yourVar.getClass().getDeclaredConstructor().newInstance();
+   public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+       var myVar = new Object();
+       var yourVar = new Object(){public Integer doit(){return 1;}};
+       var theirVar = new Object(){public Integer doit(){return 1;}};
+       var otherVar = yourVar.getClass().getDeclaredConstructor().newInstance();
 
-        String v1 = myVar.getClass().equals(yourVar.getClass())    ? "a" : "b";
-        String v2 = yourVar.getClass().equals(theirVar.getClass()) ? "c" : "d";
-        String v3 = otherVar.getClass().equals(yourVar.getClass()) ? "e" : "f";
-        String v4 = yourVar.doit().equals(otherVar.doit())         ? "g" : "h";
+var x = (Object) null;
 
-        System.out.println(join(":", v1, v2, v3, v4));
+       String v1 = myVar.getClass().equals(yourVar.getClass())    ? "a" : "b";
+       String v2 = yourVar.getClass().equals(theirVar.getClass()) ? "c" : "d";
+       String v3 = otherVar.getClass().equals(yourVar.getClass()) ? "e" : "f";
+       String v4 = yourVar.doit().equals(otherVar.doit())         ? "g" : "h";
 
-        // anonymous inner classes
-        // cons of using var
-        // why did Java use var?
-    }
+       System.out.println(join(":", v1, v2, v3, v4));
+
+       // anonymous inner classes
+       // cons of using var
+       // why did Java use var?
+   }
 }
