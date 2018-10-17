556e49f87a109281026910c3942d2dd597dc1ea6
Barry Evans <barry.evans@version1.com>
2018-10-17 22:55:06 +0100

    Add breaking bad to FunctionalInterfaceChallenge

--- src/main/java/the/java/challenges/FunctionalInterfaceChallenge.java
+++ src/main/java/the/java/challenges/FunctionalInterfaceChallenge.java
@@ -4,22 +4,27 @@ import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+// A - -2
+// B - 6
+// C - 10
+// D - java.lang.ArithmeticException: / by zero
+
 public class FunctionalInterfaceChallenge {
     public static void main(String[] breakingBad) {
-        BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
-        Function<Integer, Integer> sub = (a) -> a - 2;
-        Function<Integer, Integer> mult = (a) -> a * 3;
+        BiFunction<Integer, Integer, Integer> walter = (a, b) -> a + b;
+        Function<Integer, Integer> jesse = a -> a - 2;
+        Function<Integer, Integer> mike = b -> b * 3;
 
-        BiConsumer<Integer, Integer> consumer = (a, b)
-                -> System.out.println(a + 2 + b + 2);
+        BiConsumer<Integer, Integer> gustavo = (a, b)
+                -> System.out.println(a + 2 / b + 2);
 
-        BiFunction<Integer, Integer, Integer> func =
-                add.andThen(sub).andThen(sub).andThen(sub);
-        int firstResult = func.apply(2, 2);
+        BiFunction<Integer, Integer, Integer> saul =
+                walter.andThen(jesse).andThen(jesse).andThen(jesse);
+        int firstLaunder = saul.apply(2, 2);
 
-        add.andThen(sub).andThen(mult);
-        int secondResult = add.apply(2, 2);
+        walter.andThen(jesse).andThen(mike);
+        int secondLaunder = walter.apply(2, 2);
 
-        consumer.accept(firstResult, secondResult);
+        gustavo.accept(firstLaunder, secondLaunder);
     }
 }
