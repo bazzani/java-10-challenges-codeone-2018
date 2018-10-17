2b2bfda667f0b3daa370befaa061050deaa5234e
Barry Evans <barry.evans@version1.com>
2018-10-17 23:47:36 +0100

    Remove andThen

--- src/main/java/the/java/challenges/FunctionalInterfaceChallenge.java
+++ src/main/java/the/java/challenges/FunctionalInterfaceChallenge.java
@@ -6,25 +6,26 @@ import java.util.function.Function;
 
 // A - -2
 // B - 6
-// C - 10
+// C - 4
 // D - java.lang.ArithmeticException: / by zero
 
 public class FunctionalInterfaceChallenge {
     public static void main(String[] breakingBad) {
-        BiFunction<Integer, Integer, Integer> walter = (a, b) -> a + b;
+        BiFunction<Integer, Integer, Integer> heisenberg = (a, b) -> a + b;
         Function<Integer, Integer> jesse = a -> a - 2;
         Function<Integer, Integer> mike = b -> b * 3;
 
         BiConsumer<Integer, Integer> gustavo = (a, b)
                 -> System.out.println(a + 2 / b + 2);
 
-        BiFunction<Integer, Integer, Integer> saul =
-                walter.andThen(jesse).andThen(jesse).andThen(jesse);
+        BiFunction<Integer, Integer, Integer> saul = heisenberg.andThen(jesse);
         int firstLaunder = saul.apply(2, 2);
 
-        walter.andThen(jesse).andThen(mike);
-        int secondLaunder = walter.apply(2, 2);
+        heisenberg.andThen(jesse).andThen(mike);
+        int secondLaunder = heisenberg.apply(2, 2);
 
         gustavo.accept(firstLaunder, secondLaunder);
     }
 }
+
+// line 25
