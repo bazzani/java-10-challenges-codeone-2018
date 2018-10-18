d639feec9de551d08bd702c12574439c94191dac
Barry Evans <barry.evans@version1.com>
2018-10-18 21:12:00 +0100

    Final Collection factories challenge

--- src/main/java/the/java/challenges/CollectionFactoriesChallenge.java
+++ src/main/java/the/java/challenges/CollectionFactoriesChallenge.java
@@ -1,52 +1,43 @@
 package the.java.challenges;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+// A - no space for Marvel heros here!|Where are the villains?
+// B - no space for villains, and exits
+// C - Joker, Riddler, Joker
+// D - Harley Quinn, Joker, Riddler, Joker
+
 public class CollectionFactoriesChallenge {
     public static void main(String... doYourBest) {
-        List<String> dcHeros = List.of("Batman", "Robin", "Superman", "Robin");
-        Set<String> dcVillains = Set.of("Joker", "Penguin", "Lex Luthor");
-        Map<String, String> dcBattles = Map.of("Batman", "Joker", "Robin",
-                "Penguin", "Superman", "Lex Luthor",
-                "Penguin", "Batman");
-        Set<String> herosSet = Set.copyOf(dcHeros);
+        final Collection<Object> finalDcCharacters = new ArrayList<>();
+
+        List<String> dcHeros = List.of();
+        Set<String> dcVillains = Set.of();
 
         try {
-            dcHeros.add("Wonder Woman");
+            dcHeros.add("Deadpool");
         } catch (UnsupportedOperationException e) {
-            System.out.println("no space for heros");
+            System.out.print("no space for Marvel heros here!|");
         }
 
         try {
             dcVillains.stream().collect(Collectors.toList()).add("Harley Quinn");
-        } catch (UnsupportedOperationException e) {
-            System.out.println("no space for villains");
+            finalDcCharacters.addAll(new ArrayList<>(dcVillains));
+        } catch (UnsupportedOperationException | IllegalArgumentException e) {
+            System.out.print("no space for villains");
+            System.exit(0);
         }
 
         try {
-            Set.copyOf(dcVillains).remove("Harley Quinn");
-        } catch (UnsupportedOperationException | NullPointerException e) {
-            System.out.println("Where is Harley?");
+            var villains = Set.copyOf(dcVillains);
+            finalDcCharacters.addAll(List.of(villains, Set.of("Joker", "Riddler", "Joker")));
+        } catch (IllegalArgumentException e) {
+            System.out.print("Where are the villains?");
         }
+        finalDcCharacters.forEach(System.out::println);
     }
-
-    // modify in try catch
-
-
-// https://dzone.com/articles/immutable-vs-unmodifiable-in-jdk-10
-// View Collections
-// Collections.checkedCollection,
-// Collections.synchronizedCollection
-// Collections.unmodifiableCollection
-// List.subList
-// NavigableSet.subSet
-// Map.entrySet
-// Unmodifiable Collections
-// Unmodifiable View Collections
-// Collections.unmodifiableCollection
-// Collections.unmodifiableList
-// Collectors toUnmodifiableList(), toUnmodifiableSet(), toUnmodifiableMap
 }
